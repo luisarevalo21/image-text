dd0a0f3802590b15d0c26aaac915ba90940e2578
Luis Arevalo <arevaloluis89@yahoo.com>
2019-12-04 22:43:47 -0800

    Updates

---
+++ precache-manifest.518a97a5c066af42e1b2e8e1bf391d6d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1132e8a722c0d7dff6db3142ff7dadda",
+    "url": "/image-text/index.html"
+  },
+  {
+    "revision": "df14ef7b0efb63637310",
+    "url": "/image-text/static/css/main.a28e9810.chunk.css"
+  },
+  {
+    "revision": "ddfb031d00fa383ad4a3",
+    "url": "/image-text/static/js/2.446aaa2c.chunk.js"
+  },
+  {
+    "revision": "df14ef7b0efb63637310",
+    "url": "/image-text/static/js/main.4700685f.chunk.js"
+  },
+  {
+    "revision": "8b4ccfad16f6e216e2bc",
+    "url": "/image-text/static/js/runtime-main.b04a5639.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/image-text/static/media/logo.25bf045c.svg"
+  },
+  {
+    "revision": "e1e4d019a4067a0cc8e609e8d5dbe2ae",
+    "url": "/image-text/static/media/myphoto.e1e4d019.jpg"
+  }
+]);
