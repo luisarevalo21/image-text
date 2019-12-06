f75eed25b18c07ac9949c64f6bf755efc075af5d
Luis Arevalo <arevaloluis89@yahoo.com>
2019-12-05 16:14:23 -0800

    Updates

---
+++ precache-manifest.c3f9d083b7f52afb1410b7fb2c81c66a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "63d6879a5b20902c46fec9051de9fb4d",
+    "url": "/image-text/index.html"
+  },
+  {
+    "revision": "567f2446366acbd8e2f7",
+    "url": "/image-text/static/css/main.760d221a.chunk.css"
+  },
+  {
+    "revision": "6e986af89f74cd420575",
+    "url": "/image-text/static/js/2.0f8831bb.chunk.js"
+  },
+  {
+    "revision": "567f2446366acbd8e2f7",
+    "url": "/image-text/static/js/main.b3486e71.chunk.js"
+  },
+  {
+    "revision": "8b4ccfad16f6e216e2bc",
+    "url": "/image-text/static/js/runtime-main.b04a5639.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/image-text/static/media/logo.25bf045c.svg"
+  },
+  {
+    "revision": "e1e4d019a4067a0cc8e609e8d5dbe2ae",
+    "url": "/image-text/static/media/myphoto.e1e4d019.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/image-text/precache-manifest.39426449e8f3c58d70679b04974c115e.js"
+  "/image-text/precache-manifest.c3f9d083b7f52afb1410b7fb2c81c66a.js"
 );
 
 self.addEventListener('message', (event) => {
